28ef529e5c110a7a195b334dbb46c45b9e401b18
Jung <bakkim05@gmail.com>
2020-10-09 19:48:59 -0700

    modifications

--- CallSO.c
+++ CallSO.c
@@ -1,12 +1,8 @@
-#include <stdio.h>
 #include <linux/kernel.h>
-#include <sys/sysinfo.h>
 #include <sys/utsname.h>
 
-int main () {
-    struct utsname unameData;
-    uname(&unameData);
-
-    printf("Operating System: %s\nRelease: %s\nVersion: %s\n", unameData.sysname,unameData.release,unameData.version);
+asmlinkage long CallSO(void)
+{
+    printk("Operating System: %s\nRelease: %s\nVersion: %s\n", utsname()->system, utsname()->release, utsname()->version );
     return 0;
 }
