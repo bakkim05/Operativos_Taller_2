660e7425ea7346501b2d2b46ee88adcca79f1c10
Jung <bakkim05@gmail.com>
2020-10-10 12:01:35 -0700

    bug fix

--- CallSO.c
+++ CallSO.c
@@ -3,6 +3,6 @@
 
 asmlinkage long CallSO(void)
 {
-    printk("Operating System: %s\nRelease: %s\nVersion: %s\n", utsname()->system, utsname()->release, utsname()->version );
+    printk("Operating System: %s\nRelease: %s\nVersion: %s\n", utsname()->sysname, utsname()->release, utsname()->version );
     return 0;
 }
